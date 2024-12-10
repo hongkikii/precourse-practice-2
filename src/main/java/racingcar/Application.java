47f65790a3a6c714e501ccd70a1c22e8c5c4e1a8
mimi <110226866+hongkikii@users.noreply.github.com>
2024-12-11 03:30:00 +0900

    style(Application): 불필요 공백, 코드 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,26 +1,21 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         View view = new View();
         view.showNameRequest();
-
         String carNames = Console.readLine();
         Car car = new Car(carNames);
 
         view.showCountRequest();
-
         String count = Console.readLine();
 
         view.showResultHeader();
         Game game = new Game(count, car);
         game.play();
-
-        List<String> winners = game.getWinners();
-        view.show(winners);
+        view.show(game.getWinners());
     }
 }
